34f5a5e3af3549b26dd5cea5883fa2a41f2d7d36
Remotely Chaotic <jtek.fdlt@gmail.com>
2020-03-03 19:59:03 +0100

    Version increased to 1.0.1

--- version.h
+++ version.h
@@ -6,8 +6,8 @@
 
 #define MAJOR_VERSION 1
 #define MINOR_VERSION 0
-#define PATCH_VERSION 0
-#define VERSION       1.0.0.0
-#define VERSION_DOT   "1,0,0,0"
+#define PATCH_VERSION 1
+#define VERSION       1.0.1.0
+#define VERSION_DOT   "1,0,1,0"
 
 #endif // VERSION_H
