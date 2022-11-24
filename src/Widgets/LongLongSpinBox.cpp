512c418f50b6f5c5e09567207fc8e5fea46c2a68
Remotely Chaotic <jtek.fdlt@gmail.com>
2022-11-24 14:44:10 +0100

    INT64 SpinBox implemented

---
+++ src/Widgets/LongLongSpinBox.cpp
@@ -0,0 +1,155 @@
+#include "LongLongSpinBox.h"
+#include <QLineEdit>
+
+CLongLongSpinBox::CLongLongSpinBox(QWidget* pParent) :
+  QAbstractSpinBox{pParent}
+{
+  connect(lineEdit(), &QLineEdit::textEdited, this, &CLongLongSpinBox::SlotEditFinished);
+}
+CLongLongSpinBox::~CLongLongSpinBox()
+{
+}
+
+//----------------------------------------------------------------------------------------
+//
+void CLongLongSpinBox::fixup(QString& input) const
+{
+  QAbstractSpinBox::fixup(input);
+}
+
+//----------------------------------------------------------------------------------------
+//
+qint64 CLongLongSpinBox::minimum() const
+{
+  return m_iMin;
+}
+
+//----------------------------------------------------------------------------------------
+//
+qint64 CLongLongSpinBox::maximum() const
+{
+  return m_iMax;
+}
+
+//----------------------------------------------------------------------------------------
+//
+qint64 CLongLongSpinBox::value() const
+{
+  return m_iValue;
+}
+
+//----------------------------------------------------------------------------------------
+//
+void CLongLongSpinBox::stepBy(int steps)
+{
+  qint64 iNewValue = m_iValue;
+  if (steps < 0 && iNewValue + steps > iNewValue)
+  {
+    iNewValue = std::numeric_limits<qlonglong>::min();
+  }
+  else if (steps > 0 && iNewValue + steps < iNewValue)
+  {
+    iNewValue = std::numeric_limits<qlonglong>::max();
+  }
+  else
+  {
+    iNewValue += steps;
+  }
+
+  setValue(iNewValue);
+}
+
+//----------------------------------------------------------------------------------------
+//
+void CLongLongSpinBox::setMinimum(qint64 iMin)
+{
+  if (iMin != m_iMin)
+  {
+    m_iMin = iMin;
+    emit minimumChanged(iMin);
+  }
+}
+
+//----------------------------------------------------------------------------------------
+//
+void CLongLongSpinBox::setMaximum(qint64 iMax)
+{
+  if (iMax != m_iMax)
+  {
+    m_iMax = iMax;
+    emit minimumChanged(iMax);
+  }
+}
+
+//----------------------------------------------------------------------------------------
+//
+void CLongLongSpinBox::setRange(qint64 min, qint64 max)
+{
+  setMinimum(min);
+  setMaximum(max);
+}
+
+//----------------------------------------------------------------------------------------
+//
+void CLongLongSpinBox::setValue(qint64 iValue)
+{
+  if (m_iValue != iValue)
+  {
+    lineEdit()->setText(textFromValue(iValue));
+    m_iValue = iValue;
+    emit valueChanged(iValue);
+  }
+}
+
+//----------------------------------------------------------------------------------------
+//
+qlonglong CLongLongSpinBox::valueFromText(const QString& sText) const
+{
+  return sText.toLongLong();
+}
+
+//----------------------------------------------------------------------------------------
+//
+QString CLongLongSpinBox::textFromValue(qint64 iVal) const
+{
+  return QString::number(iVal);
+}
+
+//----------------------------------------------------------------------------------------
+//
+QAbstractSpinBox::StepEnabled CLongLongSpinBox::stepEnabled() const
+{
+  return StepUpEnabled | StepDownEnabled;
+}
+
+//----------------------------------------------------------------------------------------
+//
+QValidator::State CLongLongSpinBox::validate(QString& sInput, int&) const
+{
+  bool bOk;
+  qlonglong val = sInput.toLongLong(&bOk);
+  if (!bOk) { return QValidator::Invalid; }
+
+  if (val < m_iMin || val > m_iMax)
+  {
+    return QValidator::Invalid;
+  }
+
+  return QValidator::Acceptable;
+}
+
+//----------------------------------------------------------------------------------------
+//
+void CLongLongSpinBox::SlotEditFinished()
+{
+  QString sInput = lineEdit()->text();
+  int pos = 0;
+  if (QValidator::Acceptable == validate(sInput, pos))
+  {
+    setValue(valueFromText(sInput));
+  }
+  else
+  {
+    lineEdit()->setText(textFromValue(m_iValue));
+  }
+}
---
+++ src/Widgets/LongLongSpinBox.h
@@ -0,0 +1,51 @@
+#ifndef CLONGLONGSPINBOX_H
+#define CLONGLONGSPINBOX_H
+
+#include <QAbstractSpinBox>
+
+class CLongLongSpinBox : public QAbstractSpinBox
+{
+  Q_OBJECT
+  Q_DISABLE_COPY(CLongLongSpinBox)
+  Q_PROPERTY(qint64 minimum READ minimum WRITE setMinimum NOTIFY minimumChanged)
+  Q_PROPERTY(qint64 maximum READ maximum WRITE setMaximum NOTIFY maximumChanged)
+  Q_PROPERTY(qint64 value READ value WRITE setValue NOTIFY valueChanged)
+
+public:
+  explicit CLongLongSpinBox(QWidget* pParent = nullptr);
+  ~CLongLongSpinBox() override;
+
+  void fixup(QString &input) const override;
+  qint64 minimum() const;
+  qint64 maximum() const;
+  qint64 value() const;
+
+  void stepBy(int steps) override;
+  void setMinimum(qint64 iMin);
+  void setMaximum(qint64 iMax);
+  void setRange(qint64 min, qint64 max);
+
+public slots:
+  void setValue(qint64 iValue);
+
+signals:
+  void minimumChanged(qint64 iMin);
+  void maximumChanged(qint64 iMax);
+  void valueChanged(qint64 iValue);
+
+protected:
+  virtual qlonglong valueFromText(const QString& sText) const;
+  virtual QString textFromValue(qint64 iVal) const;
+  QAbstractSpinBox::StepEnabled stepEnabled() const override;
+  QValidator::State validate(QString& sInput, int& iPos) const override;
+
+private slots:
+  void SlotEditFinished();
+
+private:
+  qint64 m_iMin = 0;
+  qint64 m_iMax = LONG_MAX;
+  qint64 m_iValue = 0;
+};
+
+#endif // CLONGLONGSPINBOX_H
