b5513cfc5c98a15438e860f525f19d5e47c289df
RemotelyChaotic <jtek.fdlt+smut@gmail.com>
2024-03-02 18:45:40 +0100

    Windows: Launcher starts engine if updater is not found

--- src/Windows/Launcher/main.cpp
+++ src/Windows/Launcher/main.cpp
@@ -1,13 +1,29 @@
 #include <Windows.h>
+#include <winbase.h>
 
 int main(int argc, char *argv[])
 {
-  STARTUPINFO info = { sizeof(info) };
-  PROCESS_INFORMATION processInfo;
-  if (CreateProcess(L"updater\\JOIPEngineUpdater.exe", L"", NULL, NULL, TRUE, 0, NULL, NULL, &info, &processInfo))
+  bool bNotFound =
+      INVALID_FILE_ATTRIBUTES == GetFileAttributes(L"updater\\JOIPEngineUpdater.exe");
+  if(bNotFound)
   {
-      CloseHandle(processInfo.hProcess); // Cleanup since you don't need this
-      CloseHandle(processInfo.hThread); // Cleanup since you don't need this
+    STARTUPINFO info = { sizeof(info) };
+    PROCESS_INFORMATION processInfo;
+    if (CreateProcess(L"bin\\JOIPEngine.exe", L"", NULL, NULL, TRUE, 0, NULL, NULL, &info, &processInfo))
+    {
+        CloseHandle(processInfo.hProcess); // Cleanup since you don't need this
+        CloseHandle(processInfo.hThread); // Cleanup since you don't need this
+    }
+  }
+  else
+  {
+    STARTUPINFO info = { sizeof(info) };
+    PROCESS_INFORMATION processInfo;
+    if (CreateProcess(L"updater\\JOIPEngineUpdater.exe", L"", NULL, NULL, TRUE, 0, NULL, NULL, &info, &processInfo))
+    {
+        CloseHandle(processInfo.hProcess); // Cleanup since you don't need this
+        CloseHandle(processInfo.hThread); // Cleanup since you don't need this
+    }
   }
   return 0;
 }
