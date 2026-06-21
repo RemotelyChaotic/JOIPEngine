8b0eb5a9c74858298b49c9778db33b9b4ee926ec
RemotelyChaotic <jtek.fdlt+smut@gmail.com>
2026-06-21 21:18:09 +0200

    Fixed Linux deployment issue with AppImage

--- cmake/LinuxDeploy.sh
+++ cmake/LinuxDeploy.sh
@@ -1,8 +1,13 @@
+#!/bin/bash
+
+export PATH="$1:$PATH"
 export QMAKE="$1/qmake"
 export APPDIR="$2"
 export QML_SOURCES_PATHS="$3"
 
-export LD_LIBRARY_PATH="$2/usr/lib"
-export EXTRA_QT_MODULES="multimediawidgets;quickwidgets;svg;websockets;xml;xmlpatterns"
+export QT_PLUGIN_PATH="$1/../plugins"
+export QML2_IMPORT_PATH="$1/../qml"
+export LD_LIBRARY_PATH="$1/../lib:$2/usr/lib"
+export EXTRA_QT_MODULES="multimediawidgets;quickwidgets;svg;webchannel;websockets;xml;xmlpatterns"
 
 ./AppImage/linuxdeploy-x86_64.AppImage --appdir $APPDIR --output appimage
--- cmake/LinuxDeployQt.sh
+++ cmake/LinuxDeployQt.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+export PATH="$1:$PATH"
 export QMAKE="$1/qmake"
 export APPDIR="$2"
 export QML_SOURCES_PATHS="$3"
 
+export QT_PLUGIN_PATH="$1/../plugins"
+export QML2_IMPORT_PATH="$1/../qml"
 export LD_LIBRARY_PATH="$1/../lib:$2/usr/lib"
-export EXTRA_QT_MODULES="multimediawidgets;quickwidgets;svg;websockets;xml;xmlpatterns"
+export EXTRA_QT_MODULES="multimediawidgets;quickwidgets;svg;webchannel;websockets;xml;xmlpatterns"
 
 ./AppImage/linuxdeploy-plugin-qt-x86_64.AppImage --appdir $APPDIR
